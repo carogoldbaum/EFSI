514c8068f428c56546530ce865c365f53f03a748
CAROLINA GOLDBAUM <46437631@ort>
2022-04-05 07:57:17 -0300

    TP con tochu

---
+++ ORTEFSI01-main/Ejercicio02.js
@@ -0,0 +1,11 @@
+//Dado un array, multiplicar todos sus valores por el que se encuentra en la posición anterior, 
+//menos al siguiente, sin modificar el array original
+
+const data = [2, 3, 5, 12, 54, 5, -1, 0, 4, 23, 66, 7];
+let numerosOrdenados = [];
+
+for (let i = 0; i < numeros.length; i++){
+    const element = numeros [i];
+
+    numerosOrdenados.sort(element);
+}
