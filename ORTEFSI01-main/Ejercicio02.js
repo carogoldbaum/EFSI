8081c575f729d836d0aa055fdea6595bcafb1919
CAROLINA GOLDBAUM <46437631@ort>
2022-04-07 12:05:02 -0300

    Clase 7-4

--- ORTEFSI01-main/Ejercicio02.js
+++ ORTEFSI01-main/Ejercicio02.js
@@ -1,11 +1,25 @@
 //Dado un array, multiplicar todos sus valores por el que se encuentra en la posición anterior, 
-//menos al siguiente, sin modificar el array original
+//menos el siguiente, sin modificar el array original
 
-const data = [2, 3, 5, 12, 54, 5, -1, 0, 4, 23, 66, 7];
-let numerosOrdenados = [];
+const numeros = [2, 3, 5, 12, 54, 5, -1, 0, 4, 23, 66, 7];
+let multiplicacion;
 
 for (let i = 0; i < numeros.length; i++){
+    if(i == 0 || 12){
+     numeros.push(0);
+    }
     const element = numeros [i];
-
-    numerosOrdenados.sort(element);
+    if (i == 0){
+        anterior = 0;
+    } 
+    else {
+    const anterior = numeros [i-1];
+    }
+    if (i == numeros.length){
+        const siguiente = numeros [i+1];
+    }
+    multiplicacion = (element * anterior) - siguiente;
+    console.log (multiplicacion);
 }
+
+//Averiguar como poner 0 en la posicio -1 y 12, para que no de NaN
