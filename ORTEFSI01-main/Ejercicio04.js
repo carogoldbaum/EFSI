5908be3d4aa2e50bebcf7b6df298ff26cfdaffc4
CAROLINA GOLDBAUM <46437631@ort>
2022-04-11 10:32:11 -0300

    clase 11/4

---
+++ ORTEFSI01-main/Ejercicio04.js
@@ -0,0 +1,3 @@
+//Clacula los años bisiestos que habra desde ahora y antes de 2150
+//¿Cuantas cartas con numero par y ngras hay en una baraja de poker?
+//Eliminar 6 cartas de la baraja al azar y calcular en promedio (haciendo 1000 iteraciones) cuanto valen las cartas (con numero) que quedan.
