258381a8d3dca714f6abc677d44fda531a615eff
carogoldbaum <85694442+carogoldbaum@users.noreply.github.com>
2022-05-23 10:03:10 -0300

    Add files via upload

--- TP1_IntroducciónJavaScript/validacion.js
+++ TP1_IntroducciónJavaScript/validacion.js
@@ -1,46 +1,77 @@
 function ValidarNota (){
+    var nota1 = parseInt(document.getElementById("matematica").value);
+    var nota2 = parseInt(document.getElementById("lengua").value);
+    var nota3 = parseInt(document.getElementById("EFSI").value);
 
-    console.log("hola mundo")
+    if(nota1 > 10 || nota1 < 1){
+        alert('Error, usted debe colocar en matemática un valor mayor a 1 y menor que 10');
+        document.getElementById("matematica").style.color="red";
 
-    var ConseguirNota = parseInt(document.getElementById("matematica").value);
-    var ConseguirNota2 = parseInt(document.getElementById("lengua").value);
-    var ConseguirNota3 = parseInt(document.getElementById("EFSI").value);
+    }
+    else if(nota2 > 10 || nota2 < 1){
+        alert('Error, usted debe colocar en lengua un valor mayor a 1 y menor que 10');
+        document.getElementById("lengua").style.color="red";
 
-    if(ConseguirNota > 10 || ConseguirNota < 1){
-
-        alert('error');
+    }
+    else if(nota3 > 10 || nota2 < 1){
+        alert('Error, usted debe colocar en EFSI un valor mayor a 1 y menor que 10');
+        document.getElementById("EFSI").style.color="red";
 
     }
     else{
-
-        alert('Nota validada');
-
-    }
-
-    if(ConseguirNota2 > 10 || ConseguirNota2 < 1){
-
-        alert('error');
-
-    }
-    else{
-
-        alert('Nota validada');
-
-    }
-
-    if(ConseguirNota3 > 10 || ConseguirNota3 < 1){
-
-        alert('error');
-
-    }
-    else{
-
-        alert('Nota validada');
-
+        document.getElementById("matematica").style.color="green";
+        document.getElementById("lengua").style.color="green";
+        document.getElementById("EFSI").style.color="green";
     }
 }
 
-function Promedio (nota1, nota2, nota3){
+function Promedio (){
+    var nota1 = parseInt(document.getElementById("matematica").value);
+    var nota2 = parseInt(document.getElementById("lengua").value);
+    var nota3 = parseInt(document.getElementById("EFSI").value);
     let num = (nota1 + nota2 + nota3) / 3;
-    return num;
+
+    document.getElementById("Promedio").innerHTML=num;
+
+    if(num >= 6){
+        document.getElementById("Promedio").style.color="green";
+    }else{
+        document.getElementById("Promedio").style.color="red";
+    }
+}
+
+function MayorNota(){
+    var nota1 = parseInt(document.getElementById("matematica").value);
+    var nota2 = parseInt(document.getElementById("lengua").value);
+    var nota3 = parseInt(document.getElementById("EFSI").value);
+
+    if(nota1 > nota2 && nota1 > nota3){
+        document.getElementById("MayorNota").innerHTML="Matematica"
+        document.getElementById("MayorNota").style.color="blue";
+    }
+    else if(nota2 > nota1 && nota2 > nota3){
+        document.getElementById("MayorNota").innerHTML="Lengua"
+        document.getElementById("MayorNota").style.color="blue";
+    }
+    else if(nota3 > nota1 && nota3 > nota2){
+        document.getElementById("MayorNota").innerHTML="EFSI"
+        document.getElementById("MayorNota").style.color="blue";
+    }
+    else if(nota1 && nota2 > nota3 && nota1 == nota2){
+        document.getElementById("MayorNota").innerHTML="Matematica y Lengua"
+        document.getElementById("MayorNota").style.color="blue";
+    }
+    else if(nota1 && nota3 > nota2 && nota1 == nota3){
+        document.getElementById("MayorNota").innerHTML="Matematica y EFSI"
+        document.getElementById("MayorNota").style.color="blue";
+    }
+    else if(nota2 && nota3 > nota1 && nota2 == nota3){
+        document.getElementById("MayorNota").innerHTML="Lengua y EFSI"
+        document.getElementById("MayorNota").style.color="blue";
+    }
+    else{
+        document.getElementById("MayorNota").innerHTML="Matematica, Lengua y EFSI"
+        document.getElementById("MayorNota").style.color="blue";
+
+    } 
 }
