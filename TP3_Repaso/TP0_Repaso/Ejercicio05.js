2c4b969d4495697abf3e696540d975c830c3f976
carogoldbaum <85694442+carogoldbaum@users.noreply.github.com>
2022-06-16 18:03:30 -0300

    Delete TP3_Repaso directory

--- TP3_Repaso/TP0_Repaso/Ejercicio05.js
+++
@@ -1,3 +0,0 @@
-//¿Cuantas cartas con numero par y negras hay en una baraja de poker?
-//Eliminar 6 cartas de la baraja al azar y calcular en promedio (haciendo 1000 iteraciones)
-// cuanto valen las cartas (con numero) que quedan.
