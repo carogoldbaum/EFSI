6482c8118af0df3053cbc0ee446a982e33852c3e
carogoldbaum <85694442+carogoldbaum@users.noreply.github.com>
2022-06-30 12:10:35 -0300

    Add files via upload

---
+++ tp4_campus/src/App.js
@@ -0,0 +1,14 @@
+
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <div >
+      <header className="App-header">
+        <Navbar></Navbar>
+      </header>
+    </div>
+  );
+}
+
+export default App;
