109cdbd84b1b30de9dbb308532069b883d28534a
Android Studio <devandroid@ort>
2022-11-03 12:11:06 -0300

    .

---
+++ tp7/src/ContextState.js
@@ -0,0 +1,90 @@
+import * as React from "react";
+import { useContext } from "react";
+
+export const initialState = {
+    loading: true,
+    compra: {
+        precio: 0,
+        lista: [],
+        eliminarId: [],
+        cantProductos: 0,
+    },
+};
+
+export const ActionTypes = {
+    SetLoading: "SET_LOADING",
+    SetCompra: "SET_COMPRA",
+    SetCompraPrecio: "SET_COMPRA_PRECIO",
+    SetCompraLista: "SET_COMPRA_LISTA",
+    SetCompraEliminarId: "SET_COMPRA_ELIMINAR_ID",
+    SetCompraCantProductos: "SET_COMPRA_CANT_PRODUCTOS"
+};
+
+export const reducer = (state = {}, action) => {
+    console.log("state", state)
+    console.log("action", action)
+    switch (action.type) {
+        case ActionTypes.SetLoading:
+            return {
+                ...state,
+                loading: action.value,
+            };
+        case ActionTypes.SetCompra:
+            return {
+                ...state,
+                compra: action.value,
+            };
+        case ActionTypes.SetCompraPrecio:
+            return {
+                ...state,
+                compra: {
+                    ...state.compra,
+                    precio: action.value,
+                }
+            };
+        case ActionTypes.SetCompraLista:
+            return {
+                ...state,
+                compra: {
+                    ...state.compra,
+                    lista: [...state.compra.lista, action.value],
+                }
+            };
+        case ActionTypes.SetCompraCantProductos:
+            return {
+                ...state,
+                compra: {
+                    ...state.compra,
+                    CantProductos: action.value,
+                }
+            };
+        case ActionTypes.SetCompraEliminarId:
+            return {
+                ...state,
+                compra: {
+                    ...state.compra,
+                    lista: state.compra.lista.filter((producto) => producto.id !== action.value)
+                }
+            };
+        default:
+            return state;
+    }
+};
+
+export const initialContext = {
+    contextState: initialState,
+    setContextState: () => { },
+};
+
+const Cont = React.createContext(initialContext);
+
+export function ContextProvider({ children, initial = initialState }) {
+    const [state, dispatch] = React.useReducer(reducer, initial);
+
+    const contextState = state;
+    const setContextState = dispatch;
+
+    return <Cont.Provider value={{ contextState, setContextState }}>{children}</Cont.Provider>;
+}
+
+export const useContextState = () => useContext(Cont);
--- tp7/src/Screens/DetalleProducto.js
+++ tp7/src/Screens/DetalleProducto.js
@@ -1,14 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { GetProductoId, GetProductos } from "../AxiosClient"
 import Estilos from "../Estilo/Estilos.css"
-import CardCasero from '../Components/CardCasero';
 import { Card, Container } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
+import { ActionTypes, useContextState } from '../ContextState'
 
 function DetalleProducto() {
+
     const params = useParams();
-    console.log(params)
+    const { contextState, setContextState } = useContextState();
+
+    console.log("parametros",params)
+
     const [producto, setProducto] = useState({});
 
     useEffect(() => {
@@ -19,39 +23,82 @@ function DetalleProducto() {
         fetchData();
     }, []);
 
-console.log("resultado useeffect", producto)
+    let existeProducto = contextState.compra.lista.find(item => item.id === params)
+
+    console.log("resultado existeProductos" ,existeProducto)
+    console.log("resultado contexstate" ,contextState)
+    console.log("resultado useeffect", producto)
     return (
-        
+
         <Container className='p-3'>
-          
-                <div className="card cardMod">
-                    <Card style={{ width: '25.5rem' }}>
-                        <Card.Img variant="top" src={producto && producto.thumbnail} />
-                        <Card.Body>
-                            <Card.Title>{producto && producto.title}</Card.Title>
-                            <Card.Text>
-                                description: {producto && producto.description}
-                                <br></br>
-                                Marca: {producto && producto.brand}
-                                <br></br>
-                                Categoria: {producto && producto.category}
-                                <br></br>
-                                Descuento: {producto && producto.discountPercentage}
-                                <br></br>
-                                Puesto: {producto && producto.rating}
-                            </Card.Text>
-                            <Card.Text>
-                               Precio: ${producto && producto.price}
-                            </Card.Text>
-                        </Card.Body>
-                        <Button type="submit">Agregar al carrito</Button>
-                    </Card>
-                </div>
-    
+
+            <div className="card cardMod">
+                <Card style={{ width: '25.5rem' }}>
+                    <Card.Img variant="top" src={producto && producto.thumbnail} />
+                    <Card.Body>
+                        <Card.Title>{producto && producto.title}</Card.Title>
+                        <Card.Text>
+                            description: {producto && producto.description}
+                            <br></br>
+                            Marca: {producto && producto.brand}
+                            <br></br>
+                            Categoria: {producto && producto.category}
+                            <br></br>
+                            Descuento: {producto && producto.discountPercentage}
+                            <br></br>
+                            Puesto: {producto && producto.rating}
+                        </Card.Text>
+                        <Card.Text>
+                            Precio: ${producto && producto.price}
+                        </Card.Text>
+                    </Card.Body>
+
+                    {
+                        existeProducto
+                            ?
+                            <>
+                                <Button  style={{ fontSize: 48 }}
+                                    text="ELIMINAR"
+                                    onClick={async () => {
+                                        console.log("hay algo")
+                                        setContextState({
+                                            type: ActionTypes.SetCompraPrecio,
+                                            value: producto.price,
+                                        });
+                                        setContextState({
+                                            type: ActionTypes.SetCompraEliminarId,
+                                            value: producto.id,
+                                        });
+                                    }}
+                                />
+                            </>
+                            :
+                            <>
+                                <Button  style={{ fontSize: 48 }}
+                                    text="AGREGAR"
+                                    onClick={async () => {
+                                        console.log("hay algo0000")
+                                        setContextState({
+                                            type: ActionTypes.SetCompraPrecio,
+                                            value: producto.price,
+                                        });
+
+                                        setContextState({
+                                            type: ActionTypes.SetCompraLista,
+                                            value: producto,
+                                        });
+                                        console.log("agregar contextstate", contextState )
+                                    }
+                                    }
+                                />
+                            </>
+                    }
+
+                </Card>
+            </div>
+
         </Container>
 
     )
 }
 export default DetalleProducto;
-
-
