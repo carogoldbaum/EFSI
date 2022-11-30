ed5a82801d78ede87c190e3465607b90989805e0
carogoldbaum <85694442+carogoldbaum@users.noreply.github.com>
2022-11-30 15:03:11 -0300

    corrección

--- tp7/src/index.js
+++ tp7/src/index.js
@@ -20,6 +20,7 @@ root.render(
         <Route path="/" element={<Layout />}>
         <Route path="/detalle/:Id" element={<DetalleProducto />}></Route>
         <Route path="/Home" element={<Home />}></Route>
+        <Route index element={<Home />}></Route>
         <Route path="/Productos" element={<PantallaProductos/>}></Route>
         <Route path="/QuienesSomos" element={<QuienesSomos />}></Route>
         <Route path="/Contacto" element={<Contacto />}></Route>
